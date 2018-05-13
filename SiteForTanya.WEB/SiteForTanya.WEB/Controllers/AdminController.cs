07df521b79a964a51c85e6e4f4a9271f0b7f7138
AlexGitHub19 <sasha_tkk@mail.ru>
2018-05-13 23:57:34 +0300

    Menu for Admin is added

--- SiteForTanya.WEB/SiteForTanya.WEB/Controllers/AdminController.cs
+++ SiteForTanya.WEB/SiteForTanya.WEB/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,19 +16,70 @@ namespace SiteForTanya.WEB.Controllers
             return View();
         }
 
+        [Authorize]
+        public ActionResult Sets()
+        {
+            ViewBag.ViewName = "AdminSets";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Images()
+        {
+            ViewBag.ViewName = "AdminImages";
+            return View();
+        }
+        [Authorize]
+        public ActionResult Blog()
+        {
+            ViewBag.ViewName = "AdminBlog";
+            return View();
+        }
+
+        [Authorize]
         public ActionResult AddSet()
         {
             ViewBag.ViewName = "AdminAddSet";
             return View();
         }
 
+        [Authorize]
         public ActionResult SaveSet(string setName, string setDescription, string resultHtml, IEnumerable<HttpPostedFileBase> uploads)
         {
+            string path = Server.MapPath("~/Content/Images/Sets/"+ setName);
+            DirectoryInfo setDirectory = new DirectoryInfo(path);
+            setDirectory.Create();
+            if (uploads != null)
+            {
+                foreach (var file in uploads)
+                {
+                    if (file != null)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        file.SaveAs(Server.MapPath("~/Content/Images/Sets/" + setName + "/" + fileName));
+                    }
+                }
+            }            
             ViewBag.ViewName = "AdminSuccessfulSetSaving";
-
-            string html = Server.HtmlDecode(resultHtml);
+            string html = resultHtml.Replace("&lt;" , "<").Replace("&gt;" , ">"); ;
             ViewBag.Alex = html;
             return View("SuccessfulSetSaving");
         }
+
+        public JsonResult CheckSetName(string setName)
+        {
+            string path = Server.MapPath("~/Content/Images/Sets/" + setName);
+            DirectoryInfo setDirectory = new DirectoryInfo(path);
+            if (setDirectory.Exists)
+            {
+                return Json(new { result = "False" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { result = "True" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
     }
 }
