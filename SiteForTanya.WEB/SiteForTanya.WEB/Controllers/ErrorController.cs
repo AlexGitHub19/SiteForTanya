7d268005044b4a30511dc04307f0224b58101ce7
AlexGitHub19 <sasha_tkk@mail.ru>
2018-07-08 21:17:42 +0300

    adding handling for 404 exception

---
+++ SiteForTanya.WEB/SiteForTanya.WEB/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteForTanya.WEB.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
